3d004171f0b5d87b212ffa58004a1135942b09ce
caro1xxx <caro1xxxhv@gmail.com>
2022-10-02 09:57:04 +0800

    home complete

---
+++ pages/home/home.js
@@ -0,0 +1,78 @@
+// pages/home/home.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    input_data: [
+      { id: 1,name:"牛奶",protein:"3.22 g",energy:"54 kcal",fat:"3.2 g",
+      img:"/image/milk.png",unique: "unique1" },
+      { id: 2,name:"鸡蛋",protein:"13.3 g",energy:"144 kcal",fat:"8.8 g", img:"/image/egg.png",unique: "unique2" },
+      { id: 8,name:"米饭",protein:"2.6 g",energy:"116 kcal",fat:"0.3 g", img:"/image/rice.png",unique: "unique8" },
+      { id: 9,name:"面条",protein:"2.6 g",energy:"116 kcal",fat:"0.3 g", img:"/image/noodle.png",unique: "unique9" },
+      { id: 3,name:"猪肉",protein:"2.4 g",energy:"395 kcal",fat:"37 g", img:"/image/pork.png",unique: "unique3" },
+      { id: 4,name:"白菜",protein:"1.5 g",energy:"18 kcal",fat:"0.1 g", img:"/image/cabbage.png",unique: "unique4" },
+      { id: 6,name:"火锅",protein:">5.23 g",energy:">472 kcal",fat:">6.17 g", img:"/image/hotput.png",unique: "unique6" },
+      { id: 7,name:"快餐",protein:">16.2 g",energy:">274 kcal",fat:">23.5 g", img:"/image/fastfood.png",unique: "unique7" },
+      { id: 5,name:"苹果",protein:"54 g",energy:"85.9 kcal",fat:"0.2 g", img:"/image/apple.png",unique: "unique5" },
+      { id:10,name:"面包",protein:"9.0 g",energy:"313 kcal",fat:"8.5 g", img:"/image/bread.png",unique: "unique10" },
+      ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad(options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage() {
+
+  }
+})
